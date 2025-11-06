160f342b575d6bb173e891ad4c14c96a677f432a
DiorHomme <chomunodisaa68@gmail.com>
2025-11-06 11:42:00 +0200

    Update App.tsx

--- HospitalApp/App.tsx
+++ HospitalApp/App.tsx
@@ -4,9 +4,13 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { enableScreens } from 'react-native-screens';
 
 // Import your navigation
 import AppNavigator from './src/navigation/AppNavigator';
+
+// Optimize memory usage and performance
+enableScreens();
 
 export default function App() {
   return (
