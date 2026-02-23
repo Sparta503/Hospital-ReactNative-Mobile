d255ea5bb4ec3b36617221002c1150d65a60ab9b
DiorHomme <chomunodisaa68@gmail.com>
2026-02-23 23:52:00 +0200

    Update babel.config.js

--- HospitalApp/babel.config.js
+++ HospitalApp/babel.config.js
@@ -1,6 +1,14 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['@babel/plugin-transform-flow-strip-types'],
+      ['module-resolver', {
+        alias: {
+          'react-native/src/private/specs/components/DebuggingOverlayNativeComponent': 'react-native/Libraries/ReactNative/EmptyComponent'
+        }
+      }]
+    ]
   };
 };
